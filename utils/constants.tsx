a4c0f033e5150604526bd51b8e8c77e23b36de80
Freedman J Nicolas <100964607+KJNicolas@users.noreply.github.com>
2022-07-11 09:17:25 -0700

    Main

---
+++ utils/constants.tsx
@@ -0,0 +1,42 @@
+import { BsCode, BsEmojiSunglasses } from 'react-icons/bs';
+import { GiCakeSlice, GiGalaxy, GiLipstick } from 'react-icons/gi';
+import { FaPaw, FaMedal, FaGamepad } from 'react-icons/fa';
+
+export const topics = [
+  {
+    name: 'development',
+    icon: <BsCode />,
+  },
+  {
+    name: 'comedy',
+    icon: <BsEmojiSunglasses />,
+  },
+  {
+    name: 'gaming',
+    icon: <FaGamepad />,
+  },
+  {
+    name: 'food',
+    icon: <GiCakeSlice />,
+  },
+  {
+    name: 'dance',
+    icon: <GiGalaxy />,
+  },
+  {
+    name: 'beauty',
+    icon: <GiLipstick />,
+  },
+  {
+    name: 'animals',
+    icon: <FaPaw />,
+  },
+  {
+    name: 'sports',
+    icon: <FaMedal />,
+  },
+];
+
+export const footerList1 = ['About', 'NewsRoom', 'Store', 'Contact', 'Careers', 'Dance', 'Creator Directory']
+export const footerList2 = [ 'Afiche for Haiti','Advertise','Developers','Transparency','Afiche Rewards' ]
+export const footerList3 = [ 'Help', 'Safety', 'Terms', 'Privacy', 'Creator Portal', 'Community Guidelines' ]
